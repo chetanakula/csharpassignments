2c8bd49a8c36a6cb1bd108c4390faad7a92df4f5
chetanakula <115332203+chetanakula@users.noreply.github.com>
2022-11-13 07:13:57 +0000

    Assignment-7

---
+++ assign7.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace fileIO
+{
+    class program
+    {
+        static void Main(string[] args)
+        { 
+        string rootpath = @"C:\Users\Chetan\Source\Repos";
+        string[] dirs = Directory.GetDirectories(rootpath,"*",SearchOption.TopDirectoryOnly);
+
+       
+           foreach(string dir in dirs)
+            {
+                Console.WriteLine(dir);
+            }
+            Console.ReadLine();
+            
+            
+           
+            
+            
+            
+
+            
+        }
+        
+          
+              
+        
+    }
+}
